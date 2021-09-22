1. changes the way in which weather observations are linked to segments: now uses the weighed average of observations before and after the segment’s timestamp; 2. corrects timezone issues; 3. now uses KNMI’s FF (wind speed over prev 10 min) instead of FH (wind speed over prev 60 min) as default wind speed variable

bikeride/oikolab.py
@@ -3,6 +3,9 @@
 import json
 import requests
 import pandas as pd
+import pytz
+from timezonefinder import TimezoneFinder
+
 
 URL = 'https://api.oikolab.com/weather'
 DEFAULT_VARS = [
@@ -15,19 +18,37 @@ DEFAULT_VARS = [
 ]
 
 
-def get_oikolab(lat, lon, start, end, api_key, vars):
+def format_date(date):
+    """Format date string"""
+    if date.isnumeric():
+        date = f'{date[:4]}-{date[4:6]}-{date[6:]}'
+    return date
+
+
+def get_offset(lat, lon):
+    """Convert hour to UTC"""
+    tf = TimezoneFinder()
+    tz = tf.timezone_at(lng=lon, lat=lat)
+    sample_date = pd.to_datetime('2021-01-01')
+    utcoffset = pytz.timezone(tz).utcoffset(sample_date)
+    dstoffset = pytz.timezone(tz).dst(sample_date)
+    return utcoffset - dstoffset
+
+
+def get_oikolab(lat, lon, start, end, api_key, variables, freq):
     """Download hourly weather data from oikolab"""
-    if not vars:
-        vars = DEFAULT_VARS
+    if not variables:
+        variables = DEFAULT_VARS
     r = requests.get(
         URL,
         params={
-            'param': vars,
-            'start': start,
-            'end': end,
+            'param': variables,
+            'start': format_date(start),
+            'end': format_date(end),
             'lat': lat,
             'lon': lon,
-            'api-key': api_key
+            'api-key': api_key,
+            'freq': freq
         }
     )
     weather_data = json.loads(r.json()['data'])
@@ -41,6 +62,8 @@ def get_oikolab(lat, lon, start, end, api_key, vars):
         'wind_direction (deg)': 'wind_direction',
         'wind_speed (m/s)': 'wind_speed'
     })
-    df['date'] = df.index.map(lambda x: x.strftime('%Y%m%d'))
-    df['hour'] = df.index.map(lambda x: x.strftime('%H'))
+    offset = get_offset(lat, lon)
+    utc = df.index.map(lambda x: x - offset)
+    df['date'] = utc.map(lambda x: x.strftime('%Y%m%d'))
+    df['hour'] = utc.map(lambda x: x.strftime('%H'))
     return df